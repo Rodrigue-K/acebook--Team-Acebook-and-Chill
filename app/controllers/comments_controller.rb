91563a19d90209dc7ab541c3694a7809a48cfec8
Rodrigue  Kayembe <rodriguekayembe@gmail.com>
2019-02-07 15:19:54 +0000

    message commit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,30 @@
 class CommentsController < ApplicationController
   before_action :set_post
 
+  def index
+    @comment = Comment.new
+  end
+
   def create
   # building a new @comment object and assigning it in the user_name field based on the user currently logged in
-    @comment = @post.comments.build(comment_params)
-    @comment.user_id = current_user.id
+  p Comment
+  p 'hello i is here'
 
-# if the comment is saved
-    if @comment.save
-      flash[:success] = "You have placed your comment"
-      redirect_to :back
-    else
-      flash[:alert] = "Something has gone wrong"
-      render root_path
-    end
-  end
+  p @post
+    
+    @comment = @post.comment.create!(user_id:current_user_id, content:params['content'])
+    # @comment.user_id = current_user.id
+    # redirect_to post_comment_url
+
+# # if the comment is saved
+#     if @comment.save
+#       flash[:success] = "You have placed your comment"
+#       redirect_to :back
+#     else
+#       flash[:alert] = "Something has gone wrong"
+#       render root_path
+#     end
+ end
 
 
   def destroy
@@ -27,15 +37,20 @@ class CommentsController < ApplicationController
 
   private
 
-  def comment_params
-    params.require(:comment).permit(:content)
-  end
+ 
 
   def set_post
     @post = Post.find(params[:post_id])
   end
 
+
+  def current_user_id
+    current_user['id']
+  end
+
+end 
+
 # Setting the post instance vairable to the post from the Post model based on post_id params
-end
+
 
 # comments table has user_id and post_id
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190205162020) do
+ActiveRecord::Schema.define(version: 20190207120611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20190205162020) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.bigint "post_id"
+    t.string "content"
   end
 
   create_table "posts", force: :cascade do |t|
