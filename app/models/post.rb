e70dc3888ed7035e6888b8e371313104fa8959b5
Huxley_Millard <gerogiamillardccm@gmail.com>
2019-02-08 11:25:46 +0000

    added editable time function to check if posts are editable

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,13 @@ class Post < ApplicationRecord
   def pretty_message
     message.gsub(/\r/, '<br>')
   end
+
+  def editable?
+    if created_at > DateTime.now - 10.minutes
+      return true
+    else
+      return false
+    end
+  end
  
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
 
   let(:message){'hi /r its barry'}
+ 
 
   it "post belongs to a user" do
     assc = described_class.reflect_on_association(:user)
@@ -14,6 +15,20 @@ RSpec.describe Post, type: :model do
     cat.message = message
     expect(cat.pretty_message).to eql(message)
   end
+
+  describe 'editable ' do 
+
+    it 'should return a true that post is editable if made within last ten minuetes ' do
+      cat = Post.create(message: message, created_at: DateTime.now)      
+      expect(cat.editable?).to be(true)
+    end 
+    
+
+    it 'should return a false that post is editable if not made within last ten minuetes ' do
+      cat = Post.create(message: message, created_at: (DateTime.now - 20.minutes))      
+      expect(cat.editable?).to be(false)
+    end 
+  end 
 
  
 end
