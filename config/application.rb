2c7ebd7b88bb48547903a090aeaedf607c43cca2
Rodrigue  Kayembe <rodriguekayembe@gmail.com>
2019-02-08 12:53:56 +0000

    Added 404 page feature

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,6 @@
 require_relative 'boot'
 
+
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -11,8 +12,12 @@ module Acebook
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.exceptions_app = self.routes
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
+  
+  
 end
