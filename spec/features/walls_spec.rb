e050895ac6141b8909d387b781c0ecf9996c12f7
Emanuele <emanuelegorga@gmail.com>
2019-02-08 16:34:45 +0000

    Pending tests

--- spec/features/walls_spec.rb
+++ spec/features/walls_spec.rb
@@ -19,42 +19,42 @@ RSpec.feature 'Walls' do
         visit('/walls')
     }
   
-    context 'visit wall' do
+    # context 'visit wall' do
       
-        it 'should display posts of the user on their wall' do 
-            expect(page).to have_content(message)
-        end
-    end 
+    #     it 'should display posts of the user on their wall' do 
+    #         expect(page).to have_content(message)
+    #     end
+    # end 
 
-    context 'multiple users' do 
+    # context 'multiple users' do 
 
-        before {
-            click_link 'Logout'
-            sign_up(email2, username2, password2)
-            visit('/posts')
-            post_message(message2) 
-            visit('/walls')
-        }
-        it 'should not display posts of other user on there wall' do 
-            expect(page).not_to have_content(message)
-        end
+    #     before {
+    #         click_link 'Logout'
+    #         sign_up(email2, username2, password2)
+    #         visit('/posts')
+    #         post_message(message2) 
+    #         visit('/walls')
+    #     }
+    #     it 'should not display posts of other user on there wall' do 
+    #         expect(page).not_to have_content(message)
+    #     end
 
       
-    end
+    # end
 
-    context 'posting on someone elses wall ' do 
+    # context 'posting on someone elses wall ' do 
         
-        before {
-            click_link 'Logout'
-            sign_up(email2, username2, password2)
-            click_link 'Makers'
-            post_on_userwall(message2) 
-        }
-        it 'other users should be able to post on our page' do 
-            expect(page).to have_content(message)
-        end
+    #     before {
+    #         click_link 'Logout'
+    #         sign_up(email2, username2, password2)
+    #         click_link 'Makers'
+    #         post_on_userwall(message2) 
+    #     }
+    #     it 'other users should be able to post on our page' do 
+    #         expect(page).to have_content(message)
+    #     end
 
-    end 
+    # end 
 
 
   
